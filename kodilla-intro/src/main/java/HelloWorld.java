3fa4bc788dacf7b49624fcd0298d050f7496c58b
Kacper Wawrzyńczak <kacper.wawrzynczak1@gmail.com>
2021-04-02 10:26:25 +0200

    First

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {                               // [1]
+    public static void main(String[] args){                 // [2]
+        System.out.println("HelloWorld");                   // [3]
+        System.out.println("Kacper");
+        int myAge = 23;
+        System.out.println(23);
+        char LastName = 'W';
+        System.out.println('W');
+    }
+}
