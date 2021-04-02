afcdbf8840aa31e3a5f66cacf3178184a73e41d8
Kacper Wawrzyńczak <kacper.wawrzynczak1@gmail.com>
2021-04-02 12:42:27 +0200

    Module 0.8

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args){                 // [2]
+       String favoriteColour = "green";
+               int weight = 94;
+               double height = 1.89;
+               String favoriteClub = "Fc Barcelona";
+               int myAge = 23;
+               char carClass = 'b';
+        System.out.println(favoriteColour);
+        System.out.println(weight);
+        System.out.println(height);
+        System.out.println(favoriteClub);
+        System.out.println(myAge);
+        System.out.println(carClass);
+
+
+}
+}
