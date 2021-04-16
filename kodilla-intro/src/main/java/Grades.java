fb7f1fd256102764a6b450d9b3dc74b7ff20e0fe
Kacper Wawrzyńczak <kacper.wawrzynczak1@gmail.com>
2021-04-16 06:52:09 +0200

    Grades 2

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Grades {
+public class Grades {
+    private int[] grades;
+    private int size;
+    int value;
+    int result;
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+        this.value = value;
+    }
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+    public void theNewestValue() {
+        for(int i = 0; i < grades.length; i++)
+            System.out.println(grades[i]);
+    }
+    public void averageGrades() {
+        for(int i = 0, i < grades.length; i++) {
+            result = result + grades[i] / grades.length;
+        }
+    }
 }
