1244547bcb9524d98a40f6abcd2a70c440e45b26
Kacper Wawrzyńczak <kacper.wawrzynczak1@gmail.com>
2021-04-19 18:50:41 +0200

    Book

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    private String author;
+    private String title;
+    public static String book() {
+        Book book = Book.of("Adam Mickiewicz", "Pan Tadeusz"); {
+
+            return (book)
+        }
+    }
+}
