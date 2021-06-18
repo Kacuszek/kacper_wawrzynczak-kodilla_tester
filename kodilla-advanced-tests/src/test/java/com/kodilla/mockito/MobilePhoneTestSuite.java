79851eadc3463de0a5af22d18e69a2b3d9d1843e
Kacper Wawrzyńczak <kacper.wawrzynczak1@gmail.com>
2021-06-18 18:23:13 +0200

    18.06

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/MobilePhoneTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.mockito;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MobilePhoneTestSuite {
+    MobilePhone myPhone = Mockito.mock(MobilePhone.class);
+
+    @Test
+    public void testExpectation() {
+        Assertions.assertFalse(myPhone.needsCharging());
+        Mockito.when(myPhone.needsCharging()).thenReturn(true);
+        Assertions.assertTrue(myPhone.needsCharging());
+    }
+
+    @Test
+    public void shouldCallLaunchApplication() {
+        myPhone.launchApplication("Tetris4D");
+        Mockito.verify(myPhone).launchApplication("Tetris4D");
+    }
+
+}
